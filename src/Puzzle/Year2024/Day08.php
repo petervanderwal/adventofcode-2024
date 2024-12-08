2bacfc229000f4d6dacf73da1e10fadb65501556
Peter van der Wal <dev@petervanderwal.nl>
2024-12-08 10:46:14 +0100

    puzzle puzzle: 2024 day 8 part 2

--- src/Puzzle/Year2024/Day08.php
+++ src/Puzzle/Year2024/Day08.php
@@ -4,12 +4,12 @@ declare(strict_types=1);
 
 namespace App\Puzzle\Year2024;
 
-use App\Model\Direction;
 use App\Model\Grid;
 use App\Model\Point;
 use PeterVanDerWal\AdventOfCode\Cli\Attribute\Puzzle;
 use PeterVanDerWal\AdventOfCode\Cli\Attribute\TestWithDemoInput;
 use PeterVanDerWal\AdventOfCode\Cli\Model\PuzzleInput;
+use Symfony\Component\Console\Color;
 
 class Day08
 {
@@ -32,6 +32,34 @@ class Day08
     #[TestWithDemoInput(self::DEMO_INPUT, expectedAnswer: 14)]
     public function part1(PuzzleInput $input): int
     {
+        return $this->solve($input, true);
+    }
+
+    #[Puzzle(2024, day: 8, part: 2)]
+    #[TestWithDemoInput(
+        input: <<<EOF
+            T.........
+            ...T......
+            .T........
+            ..........
+            ..........
+            ..........
+            ..........
+            ..........
+            ..........
+            ..........
+            EOF,
+        expectedAnswer: 9,
+        name: 'T-demo',
+    )]
+    #[TestWithDemoInput(self::DEMO_INPUT, expectedAnswer: 34)]
+    public function part2(PuzzleInput $input): int
+    {
+        return $this->solve($input, false);
+    }
+
+    private function solve(PuzzleInput $input, bool $singleInterference): int
+    {
         $antennaMap = Grid::read($input);
         $interferenceMap = Grid::fill(
             numberOfRows: $antennaMap->getNumberOfRows(),
@@ -44,17 +72,30 @@ class Day08
             foreach ($antennaMap->startingFrom($firstLocation, excluding: true)->whereEquals($antenna)->keys() as $otherLocation) {
                 /** @var Point $otherLocation */
 
-                foreach ($this->getInterferenceLocations($firstLocation, $otherLocation) as $interferenceLocation) {
-                    if ($interferenceMap->hasPoint($interferenceLocation)) {
-                        $interferenceMap->set($interferenceLocation, true);
-                    }
+                foreach (
+                    $this->getInterferenceLocations(
+                        $interferenceMap,
+                        $firstLocation,
+                        $otherLocation,
+                        $singleInterference,
+                    ) as $interferenceLocation
+                ) {
+                    $interferenceMap->set($interferenceLocation, true);
                 }
             }
         }
 
         if ($input->isDemoInput()) {
             echo $interferenceMap->plot(
-                fn (bool $interference, Point $point): string => $interference ? '#' : $antennaMap->get($point)
+                function (bool $interference, Point $point) use ($antennaMap): string {
+                    $antenna = $antennaMap->get($point);
+                    if (!$interference) {
+                        return $antenna;
+                    }
+                    return (new Color('green', '', ['bold']))->apply(
+                        $antenna === '.' ? '#' : $antenna
+                    );
+                }
             ) . "\n\n";
         }
 
@@ -62,13 +103,41 @@ class Day08
     }
 
     /**
-     * @return Point[]
+     * @return iterable<Point>
      */
-    private function getInterferenceLocations(Point $firstLocation, Point $otherLocation): array
+    private function getInterferenceLocations(
+        Grid $bounds,
+        Point $firstLocation,
+        Point $otherLocation,
+        bool $singleInterference
+    ): iterable {
+        if (!$singleInterference) {
+            yield $firstLocation;
+            yield $otherLocation;
+        }
+        yield from $this->getInterferenceLocationSingleDirection($bounds, $firstLocation, $otherLocation, $singleInterference);
+        yield from $this->getInterferenceLocationSingleDirection($bounds, $otherLocation, $firstLocation, $singleInterference);
+    }
+
+    /**
+     * @return iterable<Point>
+     */
+    private function getInterferenceLocationSingleDirection(Grid $bounds, Point $from, Point $to, bool $singleInterference): iterable
     {
-        return [
-            $firstLocation->mirror($otherLocation),
-            $otherLocation->mirror($firstLocation),
-        ];
+        while (true) {
+            $interference = $from->mirror($to);
+            if (!$bounds->hasPoint($interference)) {
+                break;
+            }
+
+            yield $interference;
+
+            if ($singleInterference) {
+                break;
+            }
+
+            $from = $to;
+            $to = $interference;
+        }
     }
 }
