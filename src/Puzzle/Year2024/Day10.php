92da8121a867bd301e476194c9f8cbc8228246c8
Peter van der Wal <dev@petervanderwal.nl>
2024-12-10 07:31:14 +0100

    puzzle: 2024 day 10 part 1

---
+++ src/Puzzle/Year2024/Day10.php
@@ -0,0 +1,91 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Puzzle\Year2024;
+
+use App\Model\Direction;
+use App\Model\Grid;
+use App\Model\Point;
+use PeterVanDerWal\AdventOfCode\Cli\Attribute\Puzzle;
+use PeterVanDerWal\AdventOfCode\Cli\Attribute\TestWithDemoInput;
+use PeterVanDerWal\AdventOfCode\Cli\Model\PuzzleInput;
+use Symfony\Component\Console\Color;
+
+class Day10
+{
+    private const string DEMO_INPUT = <<<EOF
+        89010123
+        78121874
+        87430965
+        96549874
+        45678903
+        32019012
+        01329801
+        10456732
+        EOF;
+
+
+    #[Puzzle(2024, day: 10, part: 1)]
+    #[TestWithDemoInput(self::DEMO_INPUT, expectedAnswer: 36)]
+    public function part1(PuzzleInput $input): int
+    {
+        $debug = $input->isDemoInput();
+
+        $map = Grid::read($input, fn (string $char, Point $point) => [
+            'height' => (int)$char,
+            'reachable' => $char === '9' ? [(string)$point] : [],
+        ]);
+
+        for ($height = 8; $height > 0; $height--) {
+            foreach ($map->where(fn ($info) => $info['height'] === $height)->keys() as $point) {
+                $map->set($point, [
+                    'height' => $height,
+                    'reachable' => $this->getPointReachable($map, $point, $height),
+                ]);
+            }
+        }
+
+        $result = 0;
+        foreach ($map->where(fn ($info) => $info['height'] === 0)->keys() as $point) {
+            $pointReachable = $this->getPointReachable($map, $point, 0);
+            $result += count(array_unique($pointReachable));
+
+            if ($debug) {
+                $map->set($point, [
+                    'height' => 0,
+                    'reachable' => $pointReachable,
+                ]);
+            }
+        }
+
+        if ($debug) {
+            $green = new Color('green', options: ['bold']);
+            $blue = new Color('blue', options: ['bold']);
+            echo $map->plot(fn (array $data) => sprintf(
+                '%s %-19s ',
+                $green->apply((string)$data['height']),
+                '(' . $blue->apply((string)count(array_unique($data['reachable']))) . ')',
+            )) . "\n\n";
+        }
+
+        return $result;
+    }
+
+    private function getPointReachable(Grid $map, Point $point, int $pointHeight): array
+    {
+        $pointReachable = [];
+        foreach (Direction::straightCases() as $direction) {
+            $uphill = $point->moveDirection($direction);
+            if (!$map->hasPoint($uphill)) {
+                continue;
+            }
+
+            $uphillData = $map->get($uphill);
+            if ($uphillData['height'] === $pointHeight + 1) {
+                $pointReachable = [...$pointReachable, ...$uphillData['reachable']];
+            }
+        }
+        return $pointReachable;
+    }
+}
