2cebf4451d781f8e5b2890c8d3faaef4ab1228e5
Peter van der Wal <dev@petervanderwal.nl>
2024-12-12 06:36:44 +0100

    puzzle: 2024 day 12 part 2

--- src/Puzzle/Year2024/Day12.php
+++ src/Puzzle/Year2024/Day12.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Puzzle\Year2024;
 
+use App\Model\DirectedPoint;
 use App\Model\Grid;
 use App\Model\Point;
 use PeterVanDerWal\AdventOfCode\Cli\Attribute\Puzzle;
@@ -58,5 +59,68 @@ class Day12
             $result += $size * $perimeter;
         }
         return $result;
+    }
+
+    #[Puzzle(2024, day: 12, part: 2)]
+    #[TestWithDemoInput(self::SMALL_DEMO, expectedAnswer: 80)]
+    #[TestWithDemoInput(self::LARGE_DEMO, expectedAnswer: 1206)]
+    public function part2(PuzzleInput $input): int
+    {
+        $result = 0;
+        foreach (
+            Grid::read($input)->getAreas(
+                fn(string $neighbour, Point $point, Grid\Area $area) => $neighbour === $area->getFirstValue(),
+            ) as $area
+        ) {
+            $size = $area->getSize();
+            $sides = $this->countSides(...$area->getOuterBorder());
+
+            if ($input->isDemoInput()) {
+                echo sprintf(
+                    "Area %s has a size of %d and the border has %d sides.\n",
+                    $area->getFirstValue(),
+                    $size,
+                    $sides
+                );
+            }
+
+            $result += $size * $sides;
+        }
+        return $result;
+    }
+
+    private function countSides(DirectedPoint ...$border): int
+    {
+        $borderIndexed = [];
+        foreach ($border as $borderPoint) {
+            $borderIndexed[(string)$borderPoint] = $borderPoint;
+        }
+
+        $seen = [];
+        $sides = 0;
+        foreach ($borderIndexed as $key => $borderPoint) {
+            if (in_array($key, $seen)) {
+                continue;
+            }
+
+            $seen[] = (string)$borderPoint;
+            $sides++;
+
+            $moveDirection = $borderPoint->direction->turnRight();
+            $movedBorder = $borderPoint->moveDirection($moveDirection);
+            while (isset($borderIndexed[(string)$movedBorder])) {
+                $seen[] = (string)$movedBorder;
+                $movedBorder = $movedBorder->moveDirection($moveDirection);
+            }
+
+            $moveDirection = $borderPoint->direction->turnLeft();
+            $movedBorder = $borderPoint->moveDirection($moveDirection);
+            while (isset($borderIndexed[(string)$movedBorder])) {
+                $seen[] = (string)$movedBorder;
+                $movedBorder = $movedBorder->moveDirection($moveDirection);
+            }
+        }
+
+        return $sides;
     }
 }
