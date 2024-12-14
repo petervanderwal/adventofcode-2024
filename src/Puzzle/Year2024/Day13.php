fe1b947c7f276231c052cc57449c791a231c863f
Peter van der Wal <dev@petervanderwal.nl>
2024-12-14 10:27:17 +0100

    puzzle: 2024 day 13 part 2 (need to verify)
    It stopped after an hour at 35%, now increased the timeout and added a catch handler. Need to verify, but will focus first on newer days puzzles

--- src/Puzzle/Year2024/Day13.php
+++ src/Puzzle/Year2024/Day13.php
@@ -4,9 +4,12 @@ declare(strict_types=1);
 
 namespace App\Puzzle\Year2024;
 
+use App\Utility\MathUtility;
+use App\Utility\NumberUtility;
 use PeterVanDerWal\AdventOfCode\Cli\Attribute\Puzzle;
 use PeterVanDerWal\AdventOfCode\Cli\Attribute\TestWithDemoInput;
 use PeterVanDerWal\AdventOfCode\Cli\Model\PuzzleInput;
+use Spatie\Async\Pool;
 
 /**
  * Xa * A + Xb * B = Xprize
@@ -22,11 +25,11 @@ use PeterVanDerWal\AdventOfCode\Cli\Model\PuzzleInput;
  * 22B = 8400 - 94A                             | 22 = Xb, 8400 = Xprize, 94 = Xa
  * and                                          |
  * 67B = 5400 - 34A                             | 67 = Yb, 5400 = Yprize, 34 = Ya
-*                                               |
+ *                                               |
  * 8400 - 94A % 22 = 0                          | 8400 = Xprize, 94 = Xa, 22 = Xb
  * and                                          |
  * 5400 - 34A % 67 = 0                          | 5400 = Yprize, 34 = Ya, 67 = Yb
-*                                               |
+ *                                               |
  * (67 * (8400 - 94A)) % (22 * 67) = 0          | 67 = Yb, 8400 = Xprize, 94 = Xa, 22 = Xb
  * and                                          |
  * (22 * (5400 - 34A)) % (22 * 67) = 0          | 22 = Xb, 5400 = Yprize, 34 = Ya, 67 = Yb
@@ -46,7 +49,7 @@ use PeterVanDerWal\AdventOfCode\Cli\Model\PuzzleInput;
  * 6298A % 1474 = 1206                          | 6298 = (Yb * Xa), 1474 = (Xb * Yb), 1206 = ((Yb * Xprize) % (Xb * Yb))
  * and                                          |
  * 748A % 1474 = 880                            | 748 = (Xb * Ya), 1474 = (Xb * Yb), 880 = ((Xb * Yprize) % (Xb * Yb))
-*                                               |
+ *                                               |
  * According to Wikipedia https://en.wikipedia.org/wiki/Modular_arithmetic#Basic_properties
  * if                                           |
  *   a1 ≡ a2  (mod m) and b1 ≡ b2  (mod m)      |
@@ -63,8 +66,19 @@ use PeterVanDerWal\AdventOfCode\Cli\Model\PuzzleInput;
  * 1150A % 1474 = 612                           | 1150 = (((Yb * Xa) + (Xb * Ya)) % (Xb * Yb)), 1474 = (Xb * Yb), 612 = (((Yb * Xprize) + (Xb * Yprize)) % (Xb * Yb))
  * =>                                           |
  * gcd(1150, 1474) = 2                          |
- * =>                                           |
+ * => Check if 612 can be divided by 2 (our gcd |
+ *    here). If it can't, then there is no      |
+ *    answer to the puzzle. Think about it, any |
+ *    1150 * A % 1474 will be an even number    |
+ *    so that can never be an uneven number.    |
+ *                                              |
+ * => Divide all values by our gcd (2 here)     |
  * 575A % 737 = 306                             |
+ * => To solve A, try any number 0..736, to     |
+ *    find the first (minimal) value for A.     |
+ *    Within this example that is 80. Then any  |
+ *    addition of 737 (so 80, 817, 1554) will   |
+ *    be possible values for A                  |
  */
 class Day13
 {
@@ -90,46 +104,102 @@ class Day13
     #[TestWithDemoInput(self::DEMO_INPUT, expectedAnswer: 480)]
     public function part1(PuzzleInput $input): int
     {
-        return $this->solve($input, 100, 0);
+        $blocks = $input->split("\n\n");
+        $cost = 0;
+        foreach ($blocks as $block) {
+            $cost += $this->solveBlock((string)$block, 100, 0);
+        }
+        return $cost;
     }
 
-//    #[Puzzle(2024, day: 13, part: 2)]
-//    public function part2(PuzzleInput $input): int
-//    {
-//        return $this->solve($input, PHP_INT_MAX, 10000000000000);
-//    }
-
-    private function solve(PuzzleInput $input, int $buttonLimit, int $offset): int
+    #[Puzzle(2024, day: 13, part: 2)]
+    public function part2(PuzzleInput $input): int
     {
+        $pool = Pool::create();
+        $pool->timeout(3600);
+
         $cost = 0;
+        $done = 0;
 
-        foreach ($input->split("\n\n") as $block) {
-            preg_match_all('/[0-9]+/', (string)$block, $matches);
-            [$Xa, $Ya, $Xb, $Yb, $Xprize, $Yprize] = array_map(fn(string $match): int => (int)$match, $matches[0]);
-            $Xprize += $offset;
-            $Yprize += $offset;
+        $blocks = $input->split("\n\n");
+        $count = count($blocks);
+        foreach ($blocks as $index => $block) {
+            $block = (string)$block;
 
-            $factor = (($Yb * $Xa) + ($Xb * $Ya)) % ($Xb * $Yb);            // 1150 in example
-            $modulo = $Xb * $Yb;                                            // 1474 in example
-            $remainder = (($Yb * $Xprize) + ($Xb * $Yprize)) % $modulo;     // 612  in example
+            $pool->add(function () use ($block) {
+                return $this->solveBlock($block, PHP_INT_MAX, 10000000000000);
+            })->then(function (int $blockCost) use (&$cost, &$done, $count) {
+                $cost += $blockCost;
+                $done++;
+                echo sprintf(
+                    "[%s] %d of %d done (%.1f%%), current cost: %d\n",
+                    date('H:i:s'),
+                    $done,
+                    $count,
+                    $done / $count * 100,
+                    $cost
+                );
+            })->catch(function (\Throwable $exception) use ($block, $index) {
+                throw new \Exception("Couldn\'t solve $index: $block\n" . $exception->getMessage(), 241215083228);
+            });
+        }
 
-            for ($aPresses = 0; $aPresses < $modulo; $aPresses++) {
-                if (($aPresses * $factor) % $modulo !== $remainder) {
+        $pool->wait();
+        return $cost;
+    }
+
+    private function solveBlock(string $block, int $buttonLimit, int $offset): int
+    {
+        [$Xa, $Ya, $Xb, $Yb, $Xprize, $Yprize] = NumberUtility::getNumbersFromLine($block);
+        $Xprize += $offset;
+        $Yprize += $offset;
+
+        $factor = (($Yb * $Xa) + ($Xb * $Ya)) % ($Xb * $Yb);            // 1150 in example
+        $modulo = $Xb * $Yb;                                            // 1474 in example
+        $remainder = (($Yb * $Xprize) + ($Xb * $Yprize)) % $modulo;     // 612  in example
+
+        $gcd = MathUtility::greatestCommonDivisor($factor, $modulo);
+        if ($remainder % $gcd !== 0) {
+            // No answer
+            return 0;
+        }
+
+        $factor /= $gcd;
+        $modulo /= $gcd;
+        $remainder /= $gcd;
+
+        for ($aPressesBase = 0; $aPressesBase < $modulo; $aPressesBase++) {
+            if (($aPressesBase * $factor) % $modulo !== $remainder) {
+                continue;
+            }
+
+            for ($multiplier = 0; ($aPresses = $aPressesBase + $multiplier * $modulo) <= $buttonLimit; $multiplier++) {
+                if (
+                    $aPresses * $Xa > $Xprize
+                    || $aPresses * $Ya > $Yprize
+                ) {
+                    break;
+                }
+
+                // $bPresses = ($Xprize - ($aPresses * $Xa)) / $Xb;
+                if (($Xprize - ($aPresses * $Xa)) % $Xb !== 0) {
                     continue;
                 }
 
-                $bPresses = (int)round(($Xprize - ($aPresses * $Xa)) / $Xb);
+                $bPresses = ($Xprize - ($aPresses * $Xa)) / $Xb;
                 if (
                     $aPresses * $Xa + $bPresses * $Xb === $Xprize
                     && $aPresses * $Ya + $bPresses * $Yb === $Yprize
-                    && $aPresses <= $buttonLimit && $bPresses <= $buttonLimit
+                    && $bPresses <= $buttonLimit
                 ) {
-                    $cost += 3 * $aPresses + $bPresses;
-                    break;
+                    return 3 * $aPresses + $bPresses;
+                    break 2;
                 }
             }
+
+            break;
         }
 
-        return $cost;
+        return 0;
     }
 }
