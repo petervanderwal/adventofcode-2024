f6874a1486b94076ec944e808081fcff698b2590
Peter van der Wal <dev@petervanderwal.nl>
2024-12-14 10:19:34 +0100

    puzzle: 2024 day 14 part 2

--- src/Puzzle/Year2024/Day14.php
+++ src/Puzzle/Year2024/Day14.php
@@ -9,6 +9,7 @@ use App\Utility\NumberUtility;
 use PeterVanDerWal\AdventOfCode\Cli\Attribute\Puzzle;
 use PeterVanDerWal\AdventOfCode\Cli\Attribute\TestWithDemoInput;
 use PeterVanDerWal\AdventOfCode\Cli\Model\PuzzleInput;
+use PeterVanDerWal\AdventOfCode\Cli\Service\FixtureService;
 
 class Day14
 {
@@ -27,40 +28,93 @@ class Day14
         p=9,5 v=-3,-3
         EOF;
 
+    private int $xSize;
+    private int $ySize;
+    private int $xSplit;
+    private int $ySplit;
+    private array $instructions;
+
+    public function __construct(
+        private readonly FixtureService $fixtureService,
+    ) {
+    }
+
+    private function init(PuzzleInput $input): void
+    {
+        $this->instructions = $input->splitMap("\n", NumberUtility::getNumbersFromLine(...));
+
+        if ($input->isDemoInput()) {
+            $this->xSize = 11;
+            $this->ySize = 7;
+        } else {
+            $this->xSize = 101;
+            $this->ySize = 103;
+        }
+        $this->xSplit = ($this->xSize - 1) / 2;
+        $this->ySplit = ($this->ySize - 1) / 2;
+    }
+
     #[Puzzle(2024, day: 14, part: 1)]
     #[TestWithDemoInput(self::DEMO_INPUT, expectedAnswer: 12)]
     public function part1(PuzzleInput $input): int
     {
+        $this->init($input);
         $seconds = 100;
 
-        if ($input->isDemoInput()) {
-            $xSize = 11;
-            $ySize = 7;
-        } else {
-            $xSize = 101;
-            $ySize = 103;
-        }
-        $xSplit = ($xSize - 1) / 2;
-        $ySplit = ($ySize - 1) / 2;
-
-        $getQuadrant = function ($x, $y) use ($xSplit, $ySplit): ?string {
-            if ($x === $xSplit || $y === $ySplit) {
+        $getQuadrant = function ($x, $y): ?string {
+            if ($x === $this->xSplit || $y === $this->ySplit) {
                 return null;
             }
-            return ($y < $ySplit ? 'T' : 'B') . ($x < $xSplit ? 'L' : 'R');
+            return ($y < $this->ySplit ? 'T' : 'B') . ($x < $this->xSplit ? 'L' : 'R');
         };
 
         $quadrants = ['TL' => 0, 'TR' => 0, 'BL' => 0, 'BR' => 0];
-        foreach ($input->splitLines() as $line) {
-            [$x, $y, $vx, $vy] = NumberUtility::getNumbersFromLine($line);
-            $x = $this->moveOnAxis($x, $vx, $seconds, $xSize);
-            $y = $this->moveOnAxis($y, $vy, $seconds, $ySize);
+        foreach ($this->instructions as [$x, $y, $vx, $vy]) {
+            $x = $this->moveOnAxis($x, $vx, $seconds, $this->xSize);
+            $y = $this->moveOnAxis($y, $vy, $seconds, $this->ySize);
             $quadrant = $getQuadrant($x, $y);
             if ($quadrant !== null) {
                 $quadrants[$quadrant]++;
             }
         }
         return ArrayUtility::multiply($quadrants);
+    }
+
+    #[Puzzle(2024, day: 14, part: 2)]
+    public function part2(PuzzleInput $input): string
+    {
+        $this->init($input);
+        $fixtureFolder = '2024/14/part-2-images/';
+        $limit = 10000;
+
+        for ($seconds = 0; $seconds < $limit; $seconds++) {
+            if ($seconds % 1000 === 0) {
+                echo sprintf("[%s] %d...\n", date('H:i:s'), $seconds);
+            }
+
+            $image = imagecreate($this->xSize, $this->ySize);
+            $white = imagecolorallocate($image, 255, 255, 255);
+            imagefill($image, 0, 0, $white);
+
+            $red = imagecolorallocate($image, 255, 0, 0);
+
+            foreach ($this->instructions as [$x, $y, $vx, $vy]) {
+                $x = $this->moveOnAxis($x, $vx, $seconds, $this->xSize);
+                $y = $this->moveOnAxis($y, $vy, $seconds, $this->ySize);
+                imagesetpixel($image, $x, $y, $red);
+            }
+
+            ob_start();
+            imagepng($image);
+            $imageContent = ob_get_clean();
+            imagedestroy($image);
+            $this->fixtureService->storeFixture($fixtureFolder . $seconds . '.png', $imageContent);
+        }
+
+        return sprintf(
+            "Open up the folder %s and find the christmas tree",
+            $this->fixtureService->getFullFilename($fixtureFolder)
+        );
     }
 
     private function moveOnAxis(int $value, int $velocity, int $seconds, int $axisLength): int
