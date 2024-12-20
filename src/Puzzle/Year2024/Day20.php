133c8690c6ea7a923220b36d44d80305c12fe967
Peter van der Wal <dev@petervanderwal.nl>
2024-12-20 08:32:39 +0100

    puzzle: 2024 day 20 part 2

--- src/Puzzle/Year2024/Day20.php
+++ src/Puzzle/Year2024/Day20.php
@@ -31,14 +31,40 @@ class Day20
         ###############
         EOF;
 
+    private Grid $map;
+
+    /**
+     * @var array<int, Point>
+     */
+    private array $track;
+
     #[Puzzle(2024, day: 20, part: 1)]
     #[TestWithDemoInput(self::DEMO_INPUT, expectedAnswer: 8)]
     public function part1(PuzzleInput $input): int
     {
         $minimumCheatScore = $input->isDemoInput() ? 12 : 100;
+        $this->init($input);
+        return $this->getCheats($minimumCheatScore);
+    }
 
+    #[Puzzle(2024, day: 20, part: 2)]
+    #[TestWithDemoInput(self::DEMO_INPUT, expectedAnswer: 3 + 4 + 22 + 12)]
+    public function part2(PuzzleInput $input): int
+    {
+        $minimumCheatScore = $input->isDemoInput() ? 70 : 100;
+        $this->init($input);
+
+        $cheats = 0;
+        for ($cheatLength = 2; $cheatLength <= 20; $cheatLength++) {
+            $cheats += $this->getCheats($minimumCheatScore, $cheatLength);
+        }
+        return $cheats;
+    }
+
+    private function init(PuzzleInput $input): void
+    {
         $start = null;
-        $map = Grid::read($input, function (string $char, Point $point) use (&$start) {
+        $this->map = Grid::read($input, function (string $char, Point $point) use (&$start) {
             if ($char === 'S') {
                 $start = $point;
                 return 0;
@@ -46,34 +72,49 @@ class Day20
             return $char;
         });
 
-        $track = [$start];
+        $this->track = [$start];
         $char = 'S';
         while ($char !== 'E') {
             foreach (Direction::straightCases() as $direction) {
                 $moved = $start->moveDirection($direction);
-                if ($map->hasPoint($moved)) {
-                    $char = $map->get($moved);
+                if ($this->map->hasPoint($moved)) {
+                    $char = $this->map->get($moved);
                     if ($char === '.' || $char === 'E') {
-                        $map->set($moved, count($track));
-                        $track[] = $start = $moved;
+                        $this->map->set($moved, count($this->track));
+                        $this->track[] = $start = $moved;
                         break;
                     }
                 }
             }
         }
+    }
 
+    private function getCheats(int $minimumCheatScore, int $exactCheatLength = 2): int
+    {
         $cheats = 0;
-        foreach ($track as $fromDistance => $point) {
-            foreach (Direction::straightCases() as $direction) {
-                $cheatTo = $point->moveDirection($direction, 2);
-                if ($map->hasPoint($cheatTo)) {
-                    $cheatToDistance = $map->get($cheatTo);
-                    if (
-                        is_int($cheatToDistance)
-                        && $cheatToDistance - $fromDistance - 2 >= $minimumCheatScore
-                    ) {
-                        $cheats++;
-                    }
+        for ($diffX = 0; $diffX <= $exactCheatLength; $diffX++) {
+            foreach ($diffX === 0 ? [0] : [-$diffX, $diffX] as $offsetX) {
+                $diffY = $exactCheatLength - $diffX;
+                foreach ($diffY === 0 ? [0] : [-$diffY, $diffY] as $offsetY) {
+                    $cheats += $this->getCheatsByOffset($minimumCheatScore, $exactCheatLength, $offsetX, $offsetY);
+                }
+            }
+        }
+        return $cheats;
+    }
+
+    private function getCheatsByOffset(int $minimumCheatScore, int $cheatLength, int $offsetX, int $offsetY): int
+    {
+        $cheats = 0;
+        foreach ($this->track as $fromDistance => $point) {
+            $cheatTo = $point->moveXY($offsetX, $offsetY);
+            if ($this->map->hasPoint($cheatTo)) {
+                $cheatToDistance = $this->map->get($cheatTo);
+                if (
+                    is_int($cheatToDistance)
+                    && $cheatToDistance - $fromDistance - $cheatLength >= $minimumCheatScore
+                ) {
+                    $cheats++;
                 }
             }
         }
