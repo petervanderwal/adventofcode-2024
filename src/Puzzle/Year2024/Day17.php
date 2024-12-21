a5bae0579cc820c12a557d1cfe661b1971205602
Peter van der Wal <dev@petervanderwal.nl>
2024-12-21 15:45:42 +0100

    puzzle: 2024 day 17 part 2

--- src/Puzzle/Year2024/Day17.php
+++ src/Puzzle/Year2024/Day17.php
@@ -8,6 +8,8 @@ use App\Utility\NumberUtility;
 use PeterVanDerWal\AdventOfCode\Cli\Attribute\Puzzle;
 use PeterVanDerWal\AdventOfCode\Cli\Attribute\TestWithDemoInput;
 use PeterVanDerWal\AdventOfCode\Cli\Model\PuzzleInput;
+use Symfony\Component\Console\Helper\ProgressBar;
+use Symfony\Component\Console\Output\ConsoleOutput;
 
 class Day17
 {
@@ -58,6 +60,135 @@ class Day17
              $this->executeInstruction();
         }
         return implode(',', $this->output);
+    }
+
+    #[Puzzle(2024, day: 17, part: 2)]
+    #[TestWithDemoInput(<<<EOF
+        Register A: 2024
+        Register B: 0
+        Register C: 0
+        
+        Program: 0,3,5,4,3,0
+        EOF, expectedAnswer: 117440)]
+    public function part2(PuzzleInput $input): int
+    {
+        $this->program = NumberUtility::getNumbersFromLine($input);
+        [, $b, $c] = array_splice($this->program, 0, 3);
+
+        // Checking some program data (and assumptions)
+        if (
+            $this->program[count($this->program) - 2] !== 3
+            || $this->program[count($this->program) - 1] !== 0
+        ) {
+            throw new \UnexpectedValueException('Last operation is not "jump back to 0"', 241221132610);
+        }
+
+        $aShifts = 0;
+        $otherShifts = 0;
+        $hasOut = false;
+        for ($i = 0; $i < count($this->program) - 2; $i += 2) {
+            $opcode = $this->program[$i];
+            $operand = $this->program[$i + 1];
+            switch ($opcode) {
+                case 0:
+                    if ($operand >= 4) {
+                        throw new \UnexpectedValueException('Combo operand for adv operation', 241221132444);
+                    }
+                    $aShifts += $operand;
+                    break;
+                case 3:
+                    throw new \UnexpectedValueException('Intermediate jnz operation', 241221132444);
+                case 5:
+                    if ($hasOut) {
+                        throw new \UnexpectedValueException('Multiple out operations', 241221132824);
+                    }
+                    $hasOut = true;
+                    break;
+                case 6:
+                case 7:
+                    // Make assumption here that if a combo operand is used, the value of the register is % 8 first
+                    $otherShifts += min(3, $operand);
+            }
+        }
+
+        if ($aShifts === 0) {
+            throw new \UnexpectedValueException('No adv operations', 241221132849);
+        }
+
+        // Get the absolute max for A -- if it was higher, the program will go another loop (and thus produce more output)
+        $absoluteMaxA = pow(2, ($aShifts * count($this->program))) - 1;
+
+        $output = new ConsoleOutput();
+        $progressBar = new ProgressBar($output->section());
+        $innerBar = new ProgressBar($output->section());
+        $progressBar->start(count($this->program));
+
+        // Get the solutions for the first digit. If we have those, the possible solutions of the 2nd digit can
+        // be determined by a step size of 8 (if $aShift = 3), the possible solutions of the 3rd digit with a step size
+        // of 64 and so on. Reason: on printing the 2nd digit, the last 3 bits of the answer doesn't matter anymore
+        // as these are shifted of the A register already.
+        $solutions = [0];
+        for ($solveFirstChars = 1; $solveFirstChars <= count($this->program); $solveFirstChars++) {
+            $newSolutions = [];
+
+            $aMax = min(
+                $absoluteMaxA,
+                1 << ($aShifts * ($solveFirstChars + 1)) << $otherShifts
+            );
+            $aIncrement = 1 << ($aShifts * ($solveFirstChars - 1));
+
+            $innerBar->start((int)ceil($aMax / $aIncrement));
+            for ($a = 0; $a < $aMax; $a += $aIncrement) {
+                foreach ($solutions as $solution) {
+                    $aTry = $solution + $a;
+                    if ($this->solvePart2With($aTry, $b, $c, $solveFirstChars)) {
+                        $newSolutions[] = $aTry;
+                    }
+                }
+                $innerBar->advance();
+            }
+
+            $innerBar->finish();
+            $progressBar->advance();
+            $progressBar->display();
+
+            if (empty($newSolutions)) {
+                throw new \UnexpectedValueException(
+                    "No solution found for the {$solveFirstChars}th character",
+                    241221145702
+                );
+            }
+            $solutions = array_unique($newSolutions);
+        }
+
+        $progressBar->finish();
+        return min($solutions);
+    }
+
+    private function solvePart2With(int $a, int $b, int $c, ?int $solveFirstChars = null): bool
+    {
+        $this->a = $a;
+        $this->b = $b;
+        $this->c = $c;
+
+        $this->output = [];
+        $verifiedOutput = 0;
+
+        $this->instructionPointer = 0;
+        while ($this->instructionPointer < count($this->program)) {
+            $this->executeInstruction();
+            if (count($this->output) > $verifiedOutput) {
+                if ($this->output[$verifiedOutput] !== ($this->program[$verifiedOutput] ?? null)) {
+                    // Early return on first invalid output
+                    return false;
+                }
+                $verifiedOutput++;
+                if ($verifiedOutput === $solveFirstChars) {
+                    return true;
+                }
+            }
+        }
+        return $this->output === $this->program;
     }
 
     private function executeInstruction(): void
