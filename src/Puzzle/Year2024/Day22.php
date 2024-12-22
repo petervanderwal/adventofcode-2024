c75fe89d8c5fb4c7546d858e51879fd148b22322
Peter van der Wal <dev@petervanderwal.nl>
2024-12-22 07:02:32 +0100

    puzzle: 2024 day 22 part 2

--- src/Puzzle/Year2024/Day22.php
+++ src/Puzzle/Year2024/Day22.php
@@ -33,11 +33,60 @@ class Day22
         return $result;
     }
 
+    #[Puzzle(2024, day: 22, part: 2)]
+    #[TestWithDemoInput("123", expectedAnswer: 6, name: '10 round')]
+    #[TestWithDemoInput("1\n2\n3\n2024", expectedAnswer: 23)]
+    public function part2(PuzzleInput $input): int
+    {
+        $rounds = 2000;
+        if ($input->isDemoInput()) {
+            $rounds = NumberUtility::getNumbersFromLine($input->demoInputName)[0] ?? $rounds;
+        }
+
+        $monkeySequenceScores = [];
+        foreach ($input->splitInt("\n") as $secret) {
+            $prices = [$secret % 10];
+            for ($i = 0; $i < $rounds; $i++) {
+                $secret = $this->getNextSecretNumber($secret);
+                $prices[] = $secret % 10;
+            }
+
+            foreach ($this->getSequenceScores(...$prices) as $sequence => $score) {
+                $monkeySequenceScores[$sequence] = ($monkeySequenceScores[$sequence] ?? 0) + $score;
+            }
+        }
+
+        return max($monkeySequenceScores);
+    }
+
     private function getNextSecretNumber(int $secret): int
     {
         $secret = ($secret ^ ($secret * 64)) % 16777216;
         $secret = ($secret ^ (int)($secret / 32)) % 16777216;
         $secret = ($secret ^ ($secret * 2048)) % 16777216;
         return $secret;
+    }
+
+    private function getSequenceScores(int ...$prices): array
+    {
+        $result = [];
+        for ($i = 4; $i < count($prices); $i++) {
+            $sequence = $this->getSequence(...array_slice($prices, $i - 4, 5));
+            if (!isset($result[$sequence])) {
+                $result[$sequence] = $prices[$i];
+            }
+        }
+        return $result;
+    }
+
+    private function getSequence(int ...$prices): string
+    {
+        $diff = [];
+        $previous = $prices[0];
+        for ($i = 1; $i < count($prices); $i++) {
+            $diff[] = $prices[$i] - $previous;
+            $previous = $prices[$i];
+        }
+        return implode(',', $diff);
     }
 }
